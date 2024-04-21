9b2415a28690bd9d7881a63f9becd89dd03217b4
Salman <mohd.salman0306@gmail.com>
2024-04-21 15:42:53 +0400

    Add google oauth 2 code for login

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
- const express = require('express');
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
 
- const app = express();
+const app = express();
 
- app.get('/', (req, res) => {
-    res.send('Hello World!')
- })
+//  app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//  })
 
- const PORT = process.env.PORT || '5001';
- 
- console.log(PORT)
+passport.use(
+   new GoogleStrategy(
+      {
+         clientID: keys.googleClientId,
+         clientSecret: keys.googleClientSecret,
+         callbackURL: '/auth/google/callback'
+      }, (accessToken, refreshToken, profile, done) => {
+         console.log(accessToken)
+         console.log(refreshToken)
+         console.log(profile)
+      }
+   )
+);
 
- app.listen(PORT);
+app.get(
+   '/auth/google', 
+   passport.authenticate('google', {
+      scope: ['profile', 'email']
+   })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+const PORT = process.env.PORT || '5001';
+
+app.listen(PORT);
