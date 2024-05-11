a833c28c32bb358bb1a282307315fada51cf6927
Salman <mohd.salman0306@gmail.com>
2024-05-11 17:14:16 +0400

    added mongoose

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+require('./services/passport');
 const keys = require('./config/keys')
 
+
+mongoose.connect(keys.mongoUri)
+
 const app = express();
+require('./routes/authRoutes')(app);
 
-//  app.get('/', (req, res) => {
-//     res.send('Hello World!')
-//  })
-
-passport.use(
-   new GoogleStrategy(
-      {
-         clientID: keys.googleClientId,
-         clientSecret: keys.googleClientSecret,
-         callbackURL: '/auth/google/callback'
-      }, (accessToken, refreshToken, profile, done) => {
-         console.log(accessToken)
-         console.log(refreshToken)
-         console.log(profile)
-      }
-   )
-);
-
-app.get(
-   '/auth/google', 
-   passport.authenticate('google', {
-      scope: ['profile', 'email']
-   })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 const PORT = process.env.PORT || '5001';
-
 app.listen(PORT);
